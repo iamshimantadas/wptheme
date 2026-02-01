1ee0eee98814ff46e5718310dce826a94bf0c0cf
SHIMANTA DAS <iamshimantadas@gmail.com>
2026-02-01 10:08:56 +0530

    array offset bug fixed

--- single.php
+++ single.php
@@ -6,8 +6,8 @@ $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'post');
 
 <br>
 
-<?php if($thumb[0]){ ?>
-<img src="<?php echo $thumb[0]; ?>" class="img-fluid" alt="">
+<?php if (is_array($thumb)) { ?>
+    <img src="<?php echo $thumb[0]; ?>" class="img-fluid" alt="">
 <?php } ?>
 
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post();
